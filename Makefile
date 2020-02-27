0c65cf91c0cf1f1c6ed980c584df3cc5f45dbf86
arubinofaux <arubinofaux@gmail.com>
2020-02-27 15:28:41 -0500

    wip

--- Makefile
+++
@@ -1,8 +0,0 @@
-build.development:
-	crystal build src/nonce.cr
-
-build.production:
-	crystal build --warnings all --stats --progress --release -o nonce src/nonce.cr
-	
-run:
-	crystal run src/nonce.cr
---
+++ src/nonce.rb
@@ -0,0 +1,33 @@
+require "bundler/setup"
+require 'yaml'
+require 'json'
+
+CONF = YAML.load_file("/root/nonce.yml")
+
+module Nonce
+  class Helper
+    def self.valid_json?(json)
+      begin
+        JSON.parse(json)
+        return true
+      rescue JSON::ParserError => e
+        return false
+      end
+    end
+  end
+
+  class BTC
+    def self.getblockchaininfo
+      data = `bitcoin-cli getblockchaininfo`
+
+      if Helper.valid_json?(data)
+        HTTParty.post("#{CONF["ping_host"]}/#{CONF["node_id"]}",
+          :body => data,
+          :headers => { 'Content-Type' => 'application/json' } 
+        )
+      end   
+    end
+  end
+end
+
+Nonce::BTC.getblockchaininfo
