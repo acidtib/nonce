c2d60d039d05d2a174e34ed3bbe563606309d486
arubinofaux <arubinofaux@gmail.com>
2020-03-22 22:58:27 -0400

    wip ltc

--- src/nonce.rb
+++ src/nonce.rb
@@ -64,6 +64,28 @@ module Nonce
       end   
     end
   end
+
+  class LTC
+    def self.getblockchaininfo
+      data = `/usr/local/bin/litecoin-cli getblockchaininfo`
+
+      if Helper.valid_json?(data)
+        j = JSON.parse(data)
+        return {
+          chain: j["chain"],
+          blocks: j["blocks"],
+          verificationprogress: j["verificationprogress"],
+          initialblockdownload: j["initialblockdownload"],
+          size_on_disk: j["size_on_disk"],
+          pruned: j["pruned"]
+        }
+      else
+        return {
+          error: "issue getting blockchain info"
+        }
+      end   
+    end
+  end
 end
 
 case CONF["blockchain"]
@@ -71,6 +93,8 @@ when "bitcoin-core"
   getblockchaininfo = Nonce::BTC.getblockchaininfo
 when "bitcoin-sv"
   getblockchaininfo = Nonce::BSV.getblockchaininfo
+when "litecoin"
+  getblockchaininfo = Nonce::LTC.getblockchaininfo
 end
 
 usage = `df -m #{CONF["path"]}`.split(/\b/)[26]
